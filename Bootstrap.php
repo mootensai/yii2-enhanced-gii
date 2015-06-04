6d883e73bb5af8c1047929cdbb69c62f03cacb59
Yohanes Candrajaya <mootensai@users.noreply.github.com>
2015-06-04 11:09:23 +0700

    Create Bootstrap.php

---
+++ Bootstrap.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2014 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace mootensai\enhancedgii;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ * @package schmunk42\giiant
+ * @author Tobias Munk <tobias@diemeisterei.de>
+ */
+class Bootstrap implements BootstrapInterface
+{
+
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        if ($app->hasModule('gii')) {
+            if (!isset($app->getModule('gii')->generators['giiant-model'])) {
+                $app->getModule('gii')->generators['enhanced-model'] = 'mootensai\enhancedgii\model\Generator';
+            }
+            if (!isset($app->getModule('gii')->generators['giiant-crud'])) {
+                $app->getModule('gii')->generators['enhanced-crud'] = 'schmunk42\enhancedgii\crud\Generator';
+            }
+        }
+    }
+}
