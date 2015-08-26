9862147f4c9d0f1dbd57d0b8351621d9657effa5
andrew <andrew@andrew.(null)>
2015-08-26 15:48:34 +0300

    fix full delete

--- crud/default/controller.php
+++ crud/default/controller.php
@@ -19,6 +19,13 @@ $urlParams = $generator->generateUrlParams();
 $actionParams = $generator->generateActionParams();
 $actionParamComments = $generator->generateActionParamComments();
 
+$relationNames = [];
+foreach ($relations as $name => $rel) {
+    if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)) {
+        $relationNames[] = $name;
+    }
+}
+
 echo "<?php\n";
 ?>
 
@@ -27,200 +34,200 @@ namespace <?= StringHelper::dirname(ltrim($generator->controllerClass, '\\')) ?>
 use Yii;
 use <?= ltrim($generator->modelClass, '\\') ?>;
 <?php if (!empty($generator->searchModelClass)): ?>
-use <?= ltrim($generator->searchModelClass, '\\') . (isset($searchModelAlias) ? " as $searchModelAlias" : "") ?>;
+    use <?= ltrim($generator->searchModelClass, '\\') . (isset($searchModelAlias) ? " as $searchModelAlias" : "") ?>;
 <?php else: ?>
-use yii\data\ActiveDataProvider;
+    use yii\data\ActiveDataProvider;
 <?php endif; ?>
 use <?= ltrim($generator->baseControllerClass, '\\') ?>;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
 /**
- * <?= $controllerClass ?> implements the CRUD actions for <?= $modelClass ?> model.
- */
+* <?= $controllerClass ?> implements the CRUD actions for <?= $modelClass ?> model.
+*/
 class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->baseControllerClass) . "\n" ?>
 {
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post'],
-                ],
-            ],
+public function behaviors()
+{
+return [
+'verbs' => [
+'class' => VerbFilter::className(),
+'actions' => [
+'delete' => ['post'],
+],
+],
 <?php if($generator->loggedUserOnly): ?>
-            'access' => [
-                'class' => \yii\filters\AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => ['index', 'view', 'create', 'update','delete'],
-                        'roles' => ['@']
-                    ],
-                    [
-                        'allow' => false
-                    ]
-                ]
-            ]
+    'access' => [
+    'class' => \yii\filters\AccessControl::className(),
+    'rules' => [
+    [
+    'allow' => true,
+    'actions' => ['index', 'view', 'create', 'update','delete'],
+    'roles' => ['@']
+    ],
+    [
+    'allow' => false
+    ]
+    ]
+    ]
 <?php endif; ?>
-        ];
-    }
+];
+}
 
-    /**
-     * Lists all <?= $modelClass ?> models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
+/**
+* Lists all <?= $modelClass ?> models.
+* @return mixed
+*/
+public function actionIndex()
+{
 <?php if (!empty($generator->searchModelClass)): ?>
-        $searchModel = new <?= isset($searchModelAlias) ? $searchModelAlias : $searchModelClass ?>();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+    $searchModel = new <?= isset($searchModelAlias) ? $searchModelAlias : $searchModelClass ?>();
+    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+    return $this->render('index', [
+    'searchModel' => $searchModel,
+    'dataProvider' => $dataProvider,
+    ]);
 <?php else: ?>
-        $dataProvider = new ActiveDataProvider([
-            'query' => <?= $modelClass ?>::find(),
-        ]);
+    $dataProvider = new ActiveDataProvider([
+    'query' => <?= $modelClass ?>::find(),
+    ]);
 
-        return $this->render('index', [
-            'dataProvider' => $dataProvider,
-        ]);
+    return $this->render('index', [
+    'dataProvider' => $dataProvider,
+    ]);
 <?php endif; ?>
-    }
+}
 
-    /**
-     * Displays a single <?= $modelClass ?> model.
-     * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return mixed
-     */
-    public function actionView(<?= $actionParams ?>)
-    {
-        $model = $this->findModel(<?= $actionParams ?>);
+/**
+* Displays a single <?= $modelClass ?> model.
+* <?= implode("\n     * ", $actionParamComments) . "\n" ?>
+* @return mixed
+*/
+public function actionView(<?= $actionParams ?>)
+{
+$model = $this->findModel(<?= $actionParams ?>);
 <?php foreach ($relations as $name => $rel): ?>
-<?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
+    <?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
         $provider<?= $rel[1]?> = new \yii\data\ArrayDataProvider([
-            'allModels' => $model-><?= $name ?>,
+        'allModels' => $model-><?= $name ?>,
         ]);
-<?php endif; ?>
+    <?php endif; ?>
 <?php endforeach; ?>
-        return $this->render('view', [
-            'model' => $this->findModel(<?= $actionParams ?>),
+return $this->render('view', [
+'model' => $this->findModel(<?= $actionParams ?>),
 <?php foreach ($relations as $name => $rel): ?>
-<?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
-            'provider<?= $rel[1]?>' => $provider<?= $rel[1]?>,
-<?php endif; ?>
+    <?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
+        'provider<?= $rel[1]?>' => $provider<?= $rel[1]?>,
+    <?php endif; ?>
 <?php endforeach; ?>
-        ]);
-    }
+]);
+}
 
+/**
+* Creates a new <?= $modelClass ?> model.
+* If creation is successful, the browser will be redirected to the 'view' page.
+* @return mixed
+*/
+public function actionCreate()
+{
+$model = new <?= $modelClass ?>();
+
+if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
+return $this->redirect(['view', <?= $urlParams ?>]);
+} else {
+return $this->render('create', [
+'model' => $model,
+]);
+}
+}
+
+/**
+* Updates an existing <?= $modelClass ?> model.
+* If update is successful, the browser will be redirected to the 'view' page.
+* <?= implode("\n     * ", $actionParamComments) . "\n" ?>
+* @return mixed
+*/
+public function actionUpdate(<?= $actionParams ?>)
+{
+$model = $this->findModel(<?= $actionParams ?>);
+
+if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll(<?php var_export($relationNames) ?>)) {
+return $this->redirect(['view', <?= $urlParams ?>]);
+} else {
+return $this->render('update', [
+'model' => $model,
+]);
+}
+}
+
+/**
+* Deletes an existing <?= $modelClass ?> model.
+* If deletion is successful, the browser will be redirected to the 'index' page.
+* <?= implode("\n     * ", $actionParamComments) . "\n" ?>
+* @return mixed
+*/
+public function actionDelete(<?= $actionParams ?>)
+{
+$this->findModel(<?= $actionParams ?>)->deleteWithRelated();
+
+return $this->redirect(['index']);
+}
+<?php if($generator->pdf):?>
     /**
-     * Creates a new <?= $modelClass ?> model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new <?= $modelClass ?>();
-
-        if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
-            return $this->redirect(['view', <?= $urlParams ?>]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Updates an existing <?= $modelClass ?> model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return mixed
-     */
-    public function actionUpdate(<?= $actionParams ?>)
-    {
-        $model = $this->findModel(<?= $actionParams ?>);
-
-        if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
-            return $this->redirect(['view', <?= $urlParams ?>]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing <?= $modelClass ?> model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return mixed
-     */
-    public function actionDelete(<?= $actionParams ?>)
-    {
-        $this->findModel(<?= $actionParams ?>)->deleteWithChildren();
-
-        return $this->redirect(['index']);
-    }
-<?php if($generator->pdf):?>    
-    /**
-     * 
-     * untuk export pdf pada saat actionView
-     *  
-     * @param type $id
-     * @return type
-     */
+    *
+    * for export pdf at actionView
+    *
+    * @param type $id
+    * @return type
+    */
     public function actionPdf($id) {
-        $model = $this->findModel($id);
-<?php foreach ($relations as $name => $rel): ?>
-<?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
-        $provider<?= $rel[1] ?> = new \yii\data\ArrayDataProvider([
+    $model = $this->findModel($id);
+    <?php foreach ($relations as $name => $rel): ?>
+        <?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
+            $provider<?= $rel[1] ?> = new \yii\data\ArrayDataProvider([
             'allModels' => $model-><?= $name;?>,
-        ]);
-<?php endif;?>
-<?php endforeach;?>
+            ]);
+        <?php endif;?>
+    <?php endforeach;?>
 
-        $content = $this->renderAjax('_pdf', [
-            'model' => $model,
-<?php foreach ($relations as $name => $rel): ?>
-<?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
+    $content = $this->renderAjax('_pdf', [
+    'model' => $model,
+    <?php foreach ($relations as $name => $rel): ?>
+        <?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
             'provider<?= $rel[1]?>' => $provider<?= $rel[1] ?>,
-<?php endif;?>
-<?php endforeach;?>
-        ]);
+        <?php endif;?>
+    <?php endforeach;?>
+    ]);
 
-        $pdf = new \kartik\mpdf\Pdf([
-            'mode' => \kartik\mpdf\Pdf::MODE_CORE,
-            'format' => \kartik\mpdf\Pdf::FORMAT_A4,
-            'orientation' => \kartik\mpdf\Pdf::ORIENT_PORTRAIT,
-            'destination' => \kartik\mpdf\Pdf::DEST_BROWSER,
-            'content' => $content,
-            'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
-            'cssInline' => '.kv-heading-1{font-size:18px}',
-            'options' => ['title' => \Yii::$app->name],
-            'methods' => [
-                'SetHeader' => [\Yii::$app->name],
-                'SetFooter' => ['{PAGENO}'],
-            ]
-        ]);
+    $pdf = new \kartik\mpdf\Pdf([
+    'mode' => \kartik\mpdf\Pdf::MODE_CORE,
+    'format' => \kartik\mpdf\Pdf::FORMAT_A4,
+    'orientation' => \kartik\mpdf\Pdf::ORIENT_PORTRAIT,
+    'destination' => \kartik\mpdf\Pdf::DEST_BROWSER,
+    'content' => $content,
+    'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
+    'cssInline' => '.kv-heading-1{font-size:18px}',
+    'options' => ['title' => \Yii::$app->name],
+    'methods' => [
+    'SetHeader' => [\Yii::$app->name],
+    'SetFooter' => ['{PAGENO}'],
+    ]
+    ]);
 
-        return $pdf->render();
+    return $pdf->render();
     }
 <?php endif; ?>
-    
-    /**
-     * Finds the <?= $modelClass ?> model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * <?= implode("\n     * ", $actionParamComments) . "\n" ?>
-     * @return <?=                   $modelClass ?> the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel(<?= $actionParams ?>)
-    {
+
+/**
+* Finds the <?= $modelClass ?> model based on its primary key value.
+* If the model is not found, a 404 HTTP exception will be thrown.
+* <?= implode("\n     * ", $actionParamComments) . "\n" ?>
+* @return <?=                   $modelClass ?> the loaded model
+* @throws NotFoundHttpException if the model cannot be found
+*/
+protected function findModel(<?= $actionParams ?>)
+{
 <?php
 if (count($pks) === 1) {
     $condition = '$id';
@@ -232,34 +239,34 @@ if (count($pks) === 1) {
     $condition = '[' . implode(', ', $condition) . ']';
 }
 ?>
-        if (($model = <?= $modelClass ?>::findOne(<?= $condition ?>)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException(<?= $generator->generateString('The requested page does not exist.')?>);
-        }
-    }
+if (($model = <?= $modelClass ?>::findOne(<?= $condition ?>)) !== null) {
+return $model;
+} else {
+throw new NotFoundHttpException(<?= $generator->generateString('The requested page does not exist.')?>);
+}
+}
 <?php foreach ($relations as $name => $rel): ?>
-<?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
-    
-    /**
-    * Action to load a tabular form grid
-    * for <?= $rel[1]."\n" ?>
-    * @author Yohanes Candrajaya <moo.tensai@gmail.com>
-    * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
-    *
-    * @return mixed
-    */
-    public function actionAdd<?= $rel[1] ?>()
-    {
+    <?php if($rel[2] && isset($rel[3]) && !in_array($name, $generator->skippedRelations)): ?>
+
+        /**
+        * Action to load a tabular form grid
+        * for <?= $rel[1]."\n" ?>
+        * @author Yohanes Candrajaya <moo.tensai@gmail.com>
+        * @author Jiwantoro Ndaru <jiwanndaru@gmail.com>
+        *
+        * @return mixed
+        */
+        public function actionAdd<?= $rel[1] ?>()
+        {
         if (Yii::$app->request->isAjax) {
-            $row = Yii::$app->request->post('<?= $rel[1] ?>');
-            if((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
-                $row[] = [];
-            return $this->renderAjax('_form<?= $rel[1] ?>', ['row' => $row]);
+        $row = Yii::$app->request->post('<?= $rel[1] ?>');
+        if((Yii::$app->request->post('isNewRecord') && Yii::$app->request->post('action') == 'load' && empty($row)) || Yii::$app->request->post('action') == 'add')
+        $row[] = [];
+        return $this->renderAjax('_form<?= $rel[1] ?>', ['row' => $row]);
         } else {
-            throw new NotFoundHttpException(<?= $generator->generateString('The requested page does not exist.')?>);
+        throw new NotFoundHttpException(<?= $generator->generateString('The requested page does not exist.')?>);
         }
-    }
-<?php endif; ?>
+        }
+    <?php endif; ?>
 <?php endforeach; ?>
 }
