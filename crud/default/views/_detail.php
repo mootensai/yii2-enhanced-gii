d7576d5ef3a229f98085d1f88e6d82b61c25581f
Yohanes Candrajaya <moo.tensai@gmail.com>
2016-03-01 08:25:21 +0700

    fixes #50

---
+++ crud/default/views/_detail.php
@@ -0,0 +1,57 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator mootensai\enhancedgii\crud\Generator */
+$urlParams = $generator->generateUrlParams();
+$pk = empty($generator->tableSchema->primaryKey) ? $generator->tableSchema->getColumnNames()[0] : $generator->tableSchema->primaryKey[0];
+
+echo "<?php\n";
+?>
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+use kartik\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
+
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-view">
+
+    <div class="row">
+        <div class="col-sm-9">
+            <h2><?= "<?= " ?>Html::encode($model-><?= $generator->getNameAttribute() ?>) ?></h2>
+        </div>
+    </div>
+
+    <div class="row">
+<?= "<?php \n" ?>
+    $gridColumn = [
+<?php 
+if (($tableSchema = $generator->getTableSchema()) === false) {
+    foreach ($generator->getColumnNames() as $name) {
+        if (++$count < 6) {
+            echo "            '" . $name . "',\n";
+        } else {
+            echo "            // '" . $name . "',\n";
+        }
+    }
+} else{
+    foreach($tableSchema->getColumnNames() as $attribute){
+        if(!in_array($attribute, $generator->skippedColumns)) {
+            echo "        ".$generator->generateDetailViewField($attribute,$generator->generateFK($tableSchema), $tableSchema);
+
+        }
+    }
+}?>
+    ];
+    echo DetailView::widget([
+        'model' => $model,
+        'attributes' => $gridColumn
+    ]); 
+?>
+    </div>
+</div>
