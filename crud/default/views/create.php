3986c1244b8ea79a6f5cc0cde213497d6b6eb647
macbook <luisarmando1234@gmail.com>
2017-12-05 14:19:00 -0600

    update

--- crud/default/views/create.php
+++ crud/default/views/create.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-create">
     <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title"><?= Html::encode($this->title) ?></h3>
+            <h3 class="box-title"><?= "<?= " ?>Html::encode($this->title) ?></h3>
         </div>
             <div class="box-body">
                 <?= "<?= " ?>$this->render('_form', [
