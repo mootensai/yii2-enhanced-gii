4b7fad729e18e7f6ab2e37a2cfa52ede10642cf7
Luis Gonzalez <luisarmando1234@gmail.com>
2018-09-19 09:55:51 -0500

    Update module.php

--- module/default/module.php
+++ module/default/module.php
@@ -21,6 +21,8 @@ namespace <?= $ns ?>;
  */
 class <?= $className ?> extends \yii\base\Module
 {
+    public $menu = [];
+
     /**
      * {@inheritdoc}
      */
@@ -32,7 +34,16 @@ class <?= $className ?> extends \yii\base\Module
     public function init()
     {
         parent::init();
-
+        Yii::configure($this, require(__DIR__ . '/config/config.php'));
+        if (isset(Yii::$app->user->identity))
+            $this->menu = ['label' => 'Opciones',
+                'visible' => !Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin,
+                'url' => ['/<?= $generator->moduleID ?>/default/index'],
+                'template' => '<a href="{url}">{label}<i class="fa fa-angle-left pull-right"></i></a>',
+                'items' => [
+                    ['label' => 'Asuntos', 'url' => ['/legal/asuntos/index'], 'visible' => Yii::$app->user->identity->isAdmin],
+                ],
+            ];
         // custom initialization code goes here
     }
 }
