2c16d33683d5250d7e6c7d76cdf2385d6b3ba8b7
Luis Gonzalez <luisarmando1234@gmail.com>
2019-01-14 00:29:37 -0600

    Create Generator.php
    Added test generator

---
+++ testsgenerator/Generator.php
@@ -0,0 +1,14 @@
+<?php
+namespace inquid\testsgenerator\migration;
+
+use Yii;
+/**
+ * This generator will generate migration file for the specified database table.
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.1
+ */
+class Generator extends \yii\gii\Generator
+{
+  
+}
