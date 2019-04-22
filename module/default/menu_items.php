dea8ab9a6033c8746078d7704428390508c79548
Luis Gonzalez <luisarmando1234@gmail.com>
2019-04-22 08:51:46 -0500

    Update menu_items.php

--- module/default/menu_items.php
+++ module/default/menu_items.php
@@ -1,8 +1,7 @@
 <?php echo "<?php
 /**
- * Created by PhpStorm.
- * User: gogl92
- * Date: 2019-04-21
- * Time: 01:38
+ * List all the controllers and actions you need in your module
  */
-return [['label' => 'Index', 'url' => [\"/{$moduleID}/index\"], 'visible' => Yii::\$app->user->identity->isAdmin]];";
+return [
+  ['label' => 'Index', 'url' => [\"/{$moduleID}/index\"], 'visible' => Yii::\$app->user->identity->isAdmin]
+];";
