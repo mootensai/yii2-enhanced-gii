56fe66b19c161ee0693c22c2a5c83d558bc7054b
gogl92 <luisarmando1234@gmail.com>
2019-07-06 02:46:06 -0500

    updated master, tests and circleci

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -1,32 +1,22 @@
 <?php
-
-namespace yiiunit\gii;
+namespace inquid\tests;
 
 use inquid\enhancedgii\module\Generator;
-use PHPUnit\Framework\TestCase;
 
 /**
  * Class ModuleTest
- * @package yiiunit\gii
+ * @package inquid\tests
  */
 class ModuleTest extends TestCase
 {
-    public function autoload()
-    {
-        require_once __DIR__ . '/../../vendor/autoload.php';
-        require_once __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-    }
-
     public function testModuleName(): void
     {
-        $this->autoload();
         $generator = new Generator();
         $this->assertEquals('INQUID Generator (Module)', $generator->getName());
     }
 
     public function testDbConnection(): void
     {
-        $this->autoload();
         $generator = new Generator();
         $generator->validateDb();
         $this->assertCount(0, $generator->getErrors());
