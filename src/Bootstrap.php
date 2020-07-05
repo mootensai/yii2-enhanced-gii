f28611922fc4d78a9b91f83ba4c5ef096440917a
gogl92 <luisarmando1234@gmail.com>
2020-07-05 16:44:07 -0500

    Updating bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -29,8 +29,6 @@ class Bootstrap implements BootstrapInterface
     {
         if ($app->hasModule('gii')) {
             if (!isset($app->getModule('gii')->generators['enhanced-gii'])) {
-                $app->getModule('gii')->generators['enhanced-gii-repo'] = 'inquid\enhancedgii\repo\Generator';
-                $app->getModule('gii')->generators['enhanced-gii-app_template'] = 'inquid\enhancedgii\app_template\Generator';
                 $app->getModule('gii')->generators['enhanced-gii-model'] = [
                     'class'     => 'inquid\enhancedgii\model\Generator',
                     'templates' => [ //setting for out templates
